4d5834fe5266f57453d3e911f4a613b70616401e
Bumizi <43131635+Bumizi@users.noreply.github.com>
2018-10-11 23:07:05 +0900

    Project 02
    2014180011
    -수업 시간에 배운 프레임워크를 적용
    -기본적인 코드 틀을 철거, 프레임워크에 맞도록 이식

---
+++ Challenge! R.P.S/main_state.py
@@ -0,0 +1,137 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import title_state
+import pause_state
+import start_state
+
+
+name = "MainState"
+
+
+player = None
+enemy = None
+grass = None
+font = None
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+
+class Player:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+class Enemy:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+class Ball21x21:
+    def __init__(self):
+        self.x, self.y = random.randint(40, 700), 600
+        self.image = load_image('ball21x21.png')
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        if self.y > 60:
+            self.y -= self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+class Ball41x41:
+    def __init__(self):
+        self.x, self.y = random.randint(40, 700), 600
+        self.image = load_image('ball41x41.png')
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        if self.y > 70:
+            self.y -= self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
+def enter():
+    global player, grass, enemy
+    player = Player()
+    enemy = Enemy()
+    grass = Grass()
+    pass
+
+
+def exit():
+    global player, grass, enemy
+    del (player)
+    del (grass)
+    del (enemy)
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.push_state(pause_state)
+        #elif event.type == SDL_MOUSEBUTTONDOWN and collision(Draw_Button_Start.x, Draw_Button_Start.y, 500, 400, event.x, event.y):
+            pass
+        #elif event.type == SDL_MOUSEBUTTONDOWN and exit_button:
+            pass
+
+
+def update():
+    player.update()
+    enemy.update()
+    pass
+
+
+def draw():
+    clear_canvas()
+    grass.draw()
+    player.draw()
+    enemy.draw()
+    update_canvas()
+    pass
---
+++ Challenge! R.P.S/title_state.py
@@ -0,0 +1,93 @@
+import game_framework
+from pico2d import *
+import main_state
+
+
+name = "TitleState"
+image = None
+
+
+class MainScreen:
+    def __init__(self):
+        self.image = load_image('RPS.png')
+
+    def draw(self):
+        self.image.draw(300, 300)
+
+
+class StartButton:
+    def __init__(self):
+        self.image = load_image('start-exit.jpg')
+        self.x, self.y = 100, 100
+
+    def draw(self):
+        self.image.clip_draw(0, 225, 950, 450, self.x, self.y)
+
+
+class ExitButton:
+    def __init__(self):
+        self.image = load_image('start-exit.jpg')
+        self.x, self.y = 500, 100
+
+    def draw(self):
+        self.image.clip_draw(0, 450, 950, 225, self.x, self.y)
+
+
+def collision(x1, y1, x2, y2, px, py):
+    if(x1 < px < x2 & y1 < py < y2):
+        return True
+
+
+def enter():
+    global image
+    image = load_image('RPS.png')
+    pass
+
+
+def exit():
+    global image
+    del(image)
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
+    pass
+
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+    pass
+
+
+
+
+
+
+
+def update():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+
+
+
